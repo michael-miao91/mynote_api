d02ff17246d5b444e5db09c4105eb54f48e8a232
zhouzaomiao <zzmiao@gmail.com>
2016-03-28 15:12:48 +0800

    add user and note nodel

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it 'should validate password' do
+    user = User.create!(username: 'test', password: 'right')
+    # user.authenticate('right').should be_true
+    # user.authenticate('wrong').should be_false
+
+  end
 end
